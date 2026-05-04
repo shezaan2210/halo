62a98a58dc45dfd41d6336d3a2879f4372f317ef
shezaan <shezaanansari2210@gmail.com>
2026-05-04 19:01:24 +0530

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import  { useState, useCallback } from 'react';
 import { ReactLenis } from 'lenis/react';
 import 'lenis/dist/lenis.css';
 import gsap from 'gsap';
